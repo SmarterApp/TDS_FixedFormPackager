0f4239a41e7a4db8c18f4d076825735bf26f923a
Ernie Muñoz <emunoz@fairwaytech.com>
2018-07-31 15:25:34 -0700

    CLI Initial implementation

---
+++ src/main/java/tds.packager/service/FixedFormPackagerService.java
@@ -0,0 +1,16 @@
+package tds.packager.service;
+
+import tds.packager.model.GitCredentials;
+
+import java.io.IOException;
+
+public interface FixedFormPackagerService {
+    /**
+     * Generates a new {@link tds.testpackage.model.TestPackage} in the file system
+     *
+     * @param inputSpreadsheetPath The path of the input spreadsheet
+     * @param outputFilePath       The path to output the generated test package file
+     * @param credentials          The gitlab credentials for the fetching the item content
+     */
+    void generateFixedFormPackage(final String inputSpreadsheetPath, final String outputFilePath, final GitCredentials credentials);
+}
