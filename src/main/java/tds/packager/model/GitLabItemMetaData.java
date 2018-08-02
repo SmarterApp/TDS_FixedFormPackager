98ab496587faba3c714eb69d74dc34e5acb73124
Steve Mock <smock@fairwaytech.com>
2018-08-02 09:57:53 -0500

    Model for GitLab metadata files.

--- src/main/java/tds/packager/model/GitLabItemMetaData.java
+++ src/main/java/tds/packager/model/GitLabItemMetaData.java
@@ -1,20 +1,16 @@
 package tds.packager.model;
 
 public class GitLabItemMetaData {
-
     private final String metadata;
     private final String itemMetadata;
 
-    public String getMetadata() {
-        return metadata;
-    }
-
-    public String getItemMetadata() {
-        return itemMetadata;
-    }
-
-    public GitLabItemMetaData(String metadata, String itemMetadata) {
+    public GitLabItemMetaData(final String metadata, final String itemMetadata) {
         this.metadata = metadata;
         this.itemMetadata = itemMetadata;
     }
+
+    public String getMetadata() { return metadata; }
+
+    public String getItemMetadata() { return itemMetadata; }
+
 }
