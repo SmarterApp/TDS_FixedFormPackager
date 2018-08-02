062509bb2386a6273d9823ffc99cfc93d9315a7a
Steve Mock <smock@fairwaytech.com>
2018-08-02 15:32:48 -0500

    Added gitlab itemrelease metadata unmarshalling.

--- src/main/java/tds/packager/service/FixedFormPackagerServiceImpl.java
+++ src/main/java/tds/packager/service/FixedFormPackagerServiceImpl.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import tds.itemrenderer.data.xml.itemrelease.Itemrelease;
 import tds.packager.model.gitlab.GitCredentials;
 import tds.support.tool.testpackage.configuration.TestPackageObjectMapperConfiguration;
 import tds.testpackage.model.TestPackage;
@@ -34,6 +35,9 @@ public class FixedFormPackagerServiceImpl implements FixedFormPackagerService {
         //TODO: get Iterable list of Item ids and pass to GitLabUtil.getItemMetaData
         String [] items = new String[] { "200-12164", "200-14286", "200-12585", "200-3453", "200-14426", "200-16433", "200-13888", "200-13923", "200-32704", "200-14241", "200-50961","200-501","200-28171"};
         final HashMap<String, GitLabItemMetaData> itemMetaData = GitLabUtil.getItemMetaData(credentials, Arrays.asList(items));
+        final ItemreleaseUnmarshaller unmarshaller = new ItemreleaseUnmarshaller();
+        Itemrelease ir = unmarshaller.unmarshallItem(itemMetaData.get(items[0]).getItemMetadata(),items[0]);
+        System.out.println("unmarshalled: " + ir.getItemPassage().getId());
 
         final TestPackage testPackage = TestPackage.builder().setId("testPackageId").build();
         final String outputFileFullPath = outputFilePath + File.separator + testPackage.getId() + ".xml";
