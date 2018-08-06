eaeb4325213147fe22ecd11d12a2338b2a825419
Mike Trupkin <mtrupkin@fairwaytech.com>
2018-08-06 14:00:34 -0700

    use spring-boot properties for gitlab credentials

--- src/test/java/tds/packager/FixedFormPackagerCommandLineRunnerTest.java
+++ src/test/java/tds/packager/FixedFormPackagerCommandLineRunnerTest.java
@@ -21,16 +21,13 @@ public class FixedFormPackagerCommandLineRunnerTest {
 
     @Before
     public void setup() {
-        runner = new FixedFormPackagerCommandLineRunner(mockPackagerService);
+        runner = new FixedFormPackagerCommandLineRunner(mockPackagerService, new GitCredentials("token", "group", "url"));
         runner.init();
     }
 
     @Test
     public void shouldCreateFixedFormPackageSuccessfullyNoOutputPath() {
-        runner.run("/path/to/spreadsheet.xlsx",
-                "-t", "myToken",
-                "-g", "myGroup",
-                "-z", "myUrl");
+        runner.run("/path/to/spreadsheet.xlsx");
 
         verify(mockPackagerService).generateFixedFormPackage(eq("/path/to/spreadsheet.xlsx"), eq("."), isA(GitCredentials.class));
     }
@@ -38,9 +35,6 @@ public class FixedFormPackagerCommandLineRunnerTest {
     @Test
     public void shouldCreateFixedFormPackageSuccessfullyWithOutputPath() {
         runner.run("/path/to/spreadsheet.xlsx",
-                "-t", "myToken",
-                "-g", "myGroup",
-                "-z", "myUrl",
                 "-o", "/output/path/");
 
         verify(mockPackagerService).generateFixedFormPackage(eq("/path/to/spreadsheet.xlsx"), eq("/output/path/"), isA(GitCredentials.class));
