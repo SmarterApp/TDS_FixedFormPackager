53079a12a1f7e79fbce694c794fba67986439b26
Steve Mock <smock@fairwaytech.com>
2018-08-09 17:33:00 -0500

    Added Itemrelease metadata to MapperBaseTest Added initial tests (need more!) to SegmentFormMapperTest

--- src/test/java/tds/packager/mapper/SegmentFormMapperTest.java
+++ src/test/java/tds/packager/mapper/SegmentFormMapperTest.java
@@ -9,7 +9,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class SegmentFormMapperTest extends MapperBaseTest {
     @Test
     public void shouldMapSegmentsForAssessment() {
-        /*
+
         List<Segment> segments = SegmentMapper.map(mockWorkbook, "SBAC-IAB-FIXED-G11M-AlgLin", this.itemMetadata);
         assertThat(segments).hasSize(2);
 
@@ -17,6 +17,6 @@ public class SegmentFormMapperTest extends MapperBaseTest {
         assertThat(segmentForms1).hasSize(1);
         SegmentForm segmentForm1 = segmentForms1.get(0);
         assertThat(segmentForm1.getId()).isEqualTo("SBAC-IAB-FIXED-G11M-AlgLinearFun-NoCalc");
-        */
+
     }
 }
