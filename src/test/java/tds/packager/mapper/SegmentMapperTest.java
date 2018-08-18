e410dca24fe79c1e0680db7266382d6ef30d4ad9
Mike Trupkin <mtrupkin@fairwaytech.com>
2018-08-17 19:06:15 -0700

    updated the sample input spread sheet used in unit tests

--- src/test/java/tds/packager/mapper/SegmentMapperTest.java
+++ src/test/java/tds/packager/mapper/SegmentMapperTest.java
@@ -12,15 +12,15 @@ public class SegmentMapperTest extends MapperBaseTest {
 
     @Test
     public void shouldMapSegmentsForAssessmentIAB() {
-        List<Segment> segments = SegmentMapper.map(mockIABWorkbook, "SBAC-IAB-FIXED-G11M", itemMetadataIAB);
+        List<Segment> segments = SegmentMapper.map(mockIABWorkbook, "SBAC-IAB-FIXED-G11M-AlgLin", itemMetadataIAB);
         assertThat(segments).hasSize(2);
 
         Segment segment1 = segments.get(0);
-        assertThat(segment1.getId()).isEqualTo("SBAC-IAB-FIXED-G11M-AlgLinearFun-NoCalc");
+        assertThat(segment1.getId()).isEqualTo("SBAC-IAB-FIXED-G11M-AlgLinearFun-NoCalc-MATH-11");
         assertThat(segment1.position()).isEqualTo(1);
         assertThat(segment1.entryApproval()).isFalse();
         assertThat(segment1.exitApproval()).isFalse();
-        assertThat(segment1.getLabel().get()).isEqualTo("SBAC-IAB-FIXED-G11M-AlgLinearFun-NoCalc");
+        assertThat(segment1.getLabel().get()).isEqualTo("(SBAC)SBAC-IAB-FIXED-G11M-AlgLinearFun-NoCalc-MATH-11-Winter-2017-2018");
         assertThat(segment1.getAlgorithmType()).isEqualTo("fixedform");
         assertThat(segment1.getAlgorithmImplementation()).isEqualTo("FAIRWAY FIXEDFORM");
 
@@ -28,18 +28,18 @@ public class SegmentMapperTest extends MapperBaseTest {
         assertThat(seg1Elements).hasSize(5);
 
         Segment segment2 = segments.get(1);
-        assertThat(segment2.getId()).isEqualTo("SBAC-IAB-FIXED-G11M-AlgLinearFun-Calc");
+        assertThat(segment2.getId()).isEqualTo("SBAC-IAB-FIXED-G11M-AlgLinearFun-Calc-MATH-11");
         assertThat(segment2.position()).isEqualTo(2);
         assertThat(segment2.entryApproval()).isFalse();
         assertThat(segment2.exitApproval()).isFalse();
-        assertThat(segment2.getLabel().get()).isEqualTo("SBAC-IAB-FIXED-G11M-AlgLinearFun-Calc");
+        assertThat(segment2.getLabel().get()).isEqualTo("(SBAC)SBAC-IAB-FIXED-G11M-AlgLinearFun-Calc-MATH-11-Winter-2017-2018");
         assertThat(segment2.getAlgorithmType()).isEqualTo("fixedform");
         assertThat(segment2.getAlgorithmImplementation()).isEqualTo("FAIRWAY FIXEDFORM");
 
         List<SegmentBlueprintElement> seg2Elements = segment2.segmentBlueprint();
         assertThat(seg2Elements).hasSize(23);
         SegmentBlueprintElement segmentBpEl = seg2Elements.stream()
-                .filter(bpEl -> bpEl.getIdRef().equals("SBAC-IAB-FIXED-G11M-AlgLinearFun-Calc"))
+                .filter(bpEl -> bpEl.getIdRef().equals("SBAC-IAB-FIXED-G11M-AlgLinearFun-Calc-MATH-11"))
                 .findFirst()
                 .get();
 
